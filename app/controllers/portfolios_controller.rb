a6d54d24be3ed194e11bb91584e2bb5c2ec47541
Martin Haisma <martinhaisma@gmail.com>
2020-02-14 22:02:43 +0100

    Implemented delete functinality for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -45,6 +45,19 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
 
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+
+    @portfolio_item.destroy
+
+    respond_to do |format|
+      format.html {
+        redirect_to portfolios_path, notice: 'Portfolio was successfully removed'
+      }
+    end
+
+  end
+
   private
 
   def portfolio_params
